ee73b46ff60b0d31e908fe304fe4535b3aaeb480
AfifJ <afifjamhari.id@gmail.com>
2025-02-04 22:34:26 +0700

    add shadcn login, dashbord (mock data)

---
+++ components/login-form.jsx
@@ -0,0 +1,50 @@
+import { Building } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+
+export function LoginForm({
+  className,
+  ...props
+}) {
+  return (
+    (<div className={cn("flex flex-col gap-6", className)} {...props}>
+      <form>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center gap-2">
+            <a href="#" className="flex flex-col items-center gap-2 font-medium">
+              <div className="flex h-24 w-24 items-center justify-center rounded-md">
+                <Building className="size-24" />
+              </div>
+              <span className="sr-only">Acme Inc.</span>
+            </a>
+            <h1 className="text-xl font-bold">Welcome to Acme Inc.</h1>
+            <div className="text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link href="/dashboard" className="underline underline-offset-4">
+                Skip Login
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+        </div>
+      </form>
+
+    </div>)
+  );
+}
