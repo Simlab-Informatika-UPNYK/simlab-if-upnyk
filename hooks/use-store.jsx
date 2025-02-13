276d0e9095ccb819b32b1e872276b1d63af3b372
AfifJ <afifjamhari.id@gmail.com>
2025-02-13 13:00:31 +0700

    remove zustand

--- hooks/use-store.jsx
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const useStore = create((set) => ({
-  aslab: null, 
-  setAslab: (aslab) => set({ aslab }), 
-  resetAslab: () => set({ aslab: null }), 
-}));
-
-export default useStore;
